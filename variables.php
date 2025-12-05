d2899ed3aea30dd27529f5bdd78babeee9d75b51
Thomas Andrei <thomasandrei1991@gmail.com>
2025-12-05 17:43:40 +0800

    Start new project

---
+++ variables.php
@@ -0,0 +1,37 @@
+<?php
+    $fullName = "George Alarcon";
+    $age = 34;
+
+    
+?>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <style>
+            h1{
+                color: blueviolet;
+                text-align: center;
+                font-family: Arial, Helvetica, sans-serif;
+                margin-top: 50px;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>Stats</h1>
+        <h1><?php echo "Name : {$fullName} " ?></h1>
+        <h1><?php echo "Age :  {$age} " ?></h1>
+    </body>
+</html>
+<?php
+// Datatypes :
+
+// String
+// Integer
+// Boolean
+// Float
+// Double
+// Character
+
+?>
